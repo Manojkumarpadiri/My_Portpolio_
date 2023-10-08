4075ab22160054080e365735ccf102cbe206440e
Manojkumarpadiri <147317383+Manojkumarpadiri@users.noreply.github.com>
2023-10-08 17:43:05 +0530

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,9 @@
+const ctaButton = document.querySelector('.cta-button');
+
+ctaButton.addEventListener('mouseover', () => {
+    ctaButton.style.backgroundColor = '#ff0000';
+});
+
+ctaButton.addEventListener('mouseout', () => {
+    ctaButton.style.backgroundColor = '#e50914';
+});
